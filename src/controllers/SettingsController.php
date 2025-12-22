32681674a18c4932982ddd8f8185d07bac1c27d5
taher-k5 <kathiriyataher16@gmail.com>
2025-12-22 12:20:21 +0530

    fix srcset and save button

--- src/controllers/SettingsController.php
+++ src/controllers/SettingsController.php
@@ -5,7 +5,6 @@ namespace SFS\craftpicturetag\controllers;
 use Craft;
 use craft\web\Controller;
 use SFS\craftpicturetag\PictureTag;
-use SFS\craftpicturetag\models\Settings;
 use yii\web\Response;
 
 
@@ -34,36 +33,4 @@ class SettingsController extends Controller
         ]);
     }
 
-    /**
-     * Saves the plugin settings.
-     */
-    public function actionSave(): ?Response
-    {
-        $this->requirePostRequest();
-        $this->requireAdmin(false);
-
-        $settings = new Settings();
-        $formData = Craft::$app->getRequest()->getBodyParam('settings', []);
-
-        if (!is_array($formData)) {
-            Craft::$app->getSession()->setError(Craft::t('picture-tag', 'Invalid settings data.'));
-            return $this->renderTemplate('picture-tag/_settings', [
-                'settings' => PictureTag::$plugin->getSettings(),
-                'readOnly' => !Craft::$app->getConfig()->getGeneral()->allowAdminChanges,
-            ]);
-        }
-
-        $settings->setAttributes($formData, false);
-
-        if ($settings->saveSettings()) {
-            Craft::$app->getSession()->setNotice(Craft::t('picture-tag', 'Settings saved.'));
-            return $this->redirectToPostedUrl();
-        }
-
-        Craft::$app->getSession()->setError(Craft::t('picture-tag', 'Couldn’t save settings.'));
-        return $this->renderTemplate('picture-tag/_settings', [
-            'settings' => $settings,
-            'readOnly' => !Craft::$app->getConfig()->getGeneral()->allowAdminChanges,
-        ]);
-    }
 }
